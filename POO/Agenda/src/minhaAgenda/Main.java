7ee4b01daaffcc66bac95b7661df7aef323beb8c
Felipe Ferreira <felipehfj@gmail.com>
2016-06-12 14:32:08 -0300

    Remoção de linha e imports desnecessários

--- POO/Agenda/src/minhaAgenda/Main.java
+++ POO/Agenda/src/minhaAgenda/Main.java
@@ -12,28 +12,13 @@ package minhaAgenda;
 public class Main {
 
     public static void main(String args[]) {
-        Pessoa p1 = new Pessoa("Felipe", "3497-2016");
-        Pessoa p2 = new Pessoa("Henrique", "99295-6072");
-        Pessoa p3 = new Pessoa("Amarelo", "99295-6072");
-        
         Agenda agenda = new Agenda();
-        
-        agenda.addContato(p1);
-        agenda.addContato(p2);
-        agenda.addContato(p3);
-        
-        //agenda.removeContato(p1);
-        
-        //agenda.imprimir();
-        //agenda.listarNome();
-        //System.out.println("");
-        //agenda.listarTelefone();
         
         agenda.popula();
         
-        //agenda.imprimir();
+        System.out.println("Ordenação por nome");
         agenda.listarNome();
-        
+        System.out.println("Ordenação por telefone");
         agenda.listarTelefone();
     }
     
