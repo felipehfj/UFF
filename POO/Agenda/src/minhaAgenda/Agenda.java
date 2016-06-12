46e78eb0046079f3eb7c7066aec530c282ae0156
Felipe Ferreira <felipehfj@gmail.com>
2016-06-12 14:30:56 -0300

    Inclusão dos métodos de geração de nome e telefone

--- POO/Agenda/src/minhaAgenda/Agenda.java
+++ POO/Agenda/src/minhaAgenda/Agenda.java
@@ -5,8 +5,8 @@
  */
 package minhaAgenda;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  *
@@ -16,51 +16,71 @@ public class Agenda {
     private ArrayList<Pessoa> contatos = new ArrayList<>();
 
     public Agenda() {
-    
+
     }
-    
-    public Pessoa addContato(Pessoa pessoa){
+
+    public Pessoa addContato(Pessoa pessoa) {
         this.contatos.add(pessoa);
         return pessoa;
     }
-    
+
     /**
      *
      * @param pessoa
      * @return
      */
-    public boolean removeContato(Pessoa pessoa){
-        if(!this.contatos.contains(pessoa)){
+    public boolean removeContato(Pessoa pessoa) {
+        if (!this.contatos.contains(pessoa)) {
             return false;
         } else {
             this.contatos.remove(pessoa);
             return true;
         }
     }
-    
-    public void listarNome(){
+
+    public void listarNome() {
         Ordenador ordem = new OrdenadorNome();
         ordem.ordena(contatos);
         this.imprimir();
     }
-    
-    public void listarTelefone(){
+
+    public void listarTelefone() {
         Ordenador ordem = new OrdenadorTelefone();
         ordem.ordena(contatos);
         this.imprimir();
     }
-    
-    public void imprimir(){
+
+    public void imprimir() {
         contatos.stream().forEach((c) -> {
             System.out.println("Nome: " + c.getNome() + " | Telefone: " + c.getTelefone());
         });
     }
-    
-    public void popula(){
-        for(int i = 0; i < 20; i++){
-            this.addContato(new Pessoa("A"+Math.random(), ""+Math.random()));
+
+    public void popula() {
+        for (int i = 0; i < 20; i++) {
+            this.addContato(new Pessoa(this.geraNome(10),this.geraTelefone(8)));
         }
     }
+
+    private String geraNome(int len) {
+        final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        SecureRandom rnd = new SecureRandom();
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            sb.append(AB.charAt(rnd.nextInt(AB.length())));
+        }
+        return sb.toString();
+    }
     
-    
+    private String geraTelefone(int len) {
+        final String tel = "0123456789";
+        SecureRandom rnd = new SecureRandom();
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            sb.append(tel.charAt(rnd.nextInt(tel.length())));
+        }
+        sb.insert(len/2, "-");
+        return sb.toString();
+    }
+
 }
